f8238b1a2f9d1dd937689ebf89a8acea88c081b0
Pika4ui <ritor87@mail.ru>
2025-12-04 02:34:08 +0300

    Update apps.js

--- apps.js
+++ apps.js
@@ -2,8 +2,14 @@ var apps = [
   {
     name: "Lampa",
     appid: "lampa",
-    url: "https://lampa.mx/",
+    url: "http://lampa.mx/",
     text: "Новое полностью бесплатное приложение для Smart TV"
   }
+  {
+    name: "DRM-play",
+    appid: "drmplay",
+    url: "http://drm-play.com",
+    text: "Ott плеер drm-play, mod плеера ott-play by Alex "
+}
   // ... другие приложения
 ];
