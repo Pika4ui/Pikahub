0451e2138c164d39284779eb64dc0c7a152ac6ad
Pika4ui <ritor87@mail.ru>
2025-12-04 02:52:13 +0300

    Update apps.js

--- apps.js
+++ apps.js
@@ -10,6 +10,18 @@ var apps = [
     appid: "drmplay",
     url: "http://drm-play.com",
     text: "Ott плеер drm-play, mod плеера ott-play by Alex "
+  },
+  {
+    name: "vokino",
+    appid: "vokino",
+    url: "http://web.vokino.pro/",
+    text: "Смотреть зарубежные фильмы и сериалы в хорошем качестве"
+  }, 
+  {
+    name: "Atodo",
+    appid: "atodo",
+    url: "http://msx.benzac.de/?start=menu:request:interaction:menu@http://atodo.fun/fun.html",
+    text: "АТОDO"
 }
   // ... другие приложения
 ];
